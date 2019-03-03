535630383c49e04588faabceafd595a0df7da05a
ankargren <sebastian.ankargren@statistik.uu.se>
2019-03-03 20:01:29 +0100

    Added new week

---
+++ veckor/vecka10_2019.R
@@ -0,0 +1,8 @@
+veckolista <- list(
+  df = tribble(
+    ~category, ~url,
+    "asiatiskt", "kycklingbullar-tandoori",
+    "asiatiskt", "sesamlax-med-egg-fried-rice",
+    "asiatiskt", "nudelsoppa-med-kokos-och-gront"
+  ),
+  vecka = 10)
