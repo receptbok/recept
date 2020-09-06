633172e9ffe8dbe07fd2a9fa0a4f6cefa3f9c4b9
ankargren <sebastian.ankargren@statistik.uu.se>
2020-09-06 15:47:56 +0200

    Added new week

---
+++ veckor/vecka36.R
@@ -0,0 +1,15 @@
+veckolista <- list(
+  df = tribble(
+    ~category, ~url,
+    "fisk", "https://www.koket.se/sesampanerad-lax-med-ponzumajonnas-och-krispig-sallad",
+    "fisk", "https://www.koket.se/sayadieh-saffransris-med-vit-fisk-och-tahinidressing",
+    "fisk", "https://www.koket.se/mitt-kok/tommy-myllymaki/stekt-fisk-med-sojadressing/",
+    "fisk", "https://www.koket.se/ugnsbakad-lax-med-fankal-och-paprika",
+    "fisk", "https://www.koket.se/nudelsoppa-med-lax-och-rod-curry",
+    
+    "vegetariskt", "https://www.dn.se/mat-dryck/broken-sushi/",
+    "vegetariskt", "https://www.dn.se/mat-dryck/quinoasallad/",
+    "vegetariskt", "https://www.koket.se/mexican-rice-bowl",
+    "vegetariskt", "https://www.koket.se/heavenly-tofu-poke-bowl"
+  ),
+  vecka = 36)
