ec086187fde9e5eb7c5b3cf0bf2f7c46bde659e1
ankargren <sebastian.ankargren@statistik.uu.se>
2020-09-29 11:31:54 +0200

    Added new week

--- veckor/vecka40.R
+++ veckor/vecka40.R
@@ -13,7 +13,7 @@ veckolista <- list(
     "fisk", "https://www.koket.se/thailandsk-lax-i-ugn-med-kokosmjolk-lime-och-ingefara",
     "vegetariskt", "https://www.koket.se/rostad-sotpotatis-med-fetaost-kikarter-och-chilimajonnas",
     "vegetariskt", "https://www.koket.se/chili-sin-carne-siri-barjes-recept",
-    "vegatriskt", "https://www.koket.se/burrito-med-stekt-ostronskivling-krispig-kal-och-jalape-oaioli",
+    "vegetariskt", "https://www.koket.se/burrito-med-stekt-ostronskivling-krispig-kal-och-jalape-oaioli",
     "asiatiskt", "https://www.koket.se/asiatiska-kottbullar-med-kokosris-och-mangosalsa"
   ),
   vecka = 40)
